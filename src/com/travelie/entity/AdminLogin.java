b1365222d704fac5fb30851e1820d1359a2b23af
Raheel Jameel <raheel_jameel@yahoo.com>
2016-12-11 03:33:54 +0500

    Created AdminLogin.java

---
+++ src/com/travelie/entity/AdminLogin.java
@@ -0,0 +1,79 @@
+package com.travelie.entity;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="adminlogin")
+public class AdminLogin {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="password")
+	private String password;
+	
+	
+	public AdminLogin(){
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setOassword(String password) {
+		this.password = password;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AdminLogin [id=" + id + ", userName=" + userName + ", password=" + password + "]";
+	}
+
+
+
+
+
+	
+	
+	
+	
+}
