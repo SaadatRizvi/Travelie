1159620148ddf57e9882801a037764118ea5a834
Raheel Jameel <raheel_jameel@yahoo.com>
2016-12-12 00:42:58 +0500

    Completed all checks in VanController.java

--- src/com/travelie/controller/VanController.java
+++ src/com/travelie/controller/VanController.java
@@ -22,6 +22,7 @@ import com.travelie.service.VanTypeService;
 @RequestMapping(value = "/van")
 public class VanController {
 	
+	boolean isUpdate = false;
 	
 	private static Logger logger = Logger
 			.getLogger(VanController.class);
@@ -31,8 +32,6 @@ public class VanController {
 	
 	@Autowired
 	VanTypeService vanTypeService;
-	
-	//
 	
 	
 @GetMapping("/list")
@@ -55,9 +54,9 @@ for (Van van : vans){
 @GetMapping("/showFormForAdd")
 public String showFormForAdd(Model theModel){
 	
+	isUpdate = false;
+	
 	Van theVan = new Van();
-	
-	
 	
 	theModel.addAttribute("van", theVan);
 	
@@ -74,28 +73,35 @@ public String saveVan(@ModelAttribute("van") Van theVan){
 
 	 List<Van> vans = vanService.getVans();
 
-		boolean isValidRegistration = true;
+	 	
+	 	// Check for Vehicle Registration
+	 	boolean isValidRegistration = true;
 		
 		
 		for (Van vanTemp : vans){
 			
-			if (theVan.getVehicleRegistration().equals(vanTemp.getVehicleRegistration())){
-				isValidRegistration = false;break;
+			if ( !( isUpdate && theVan.getId() == vanTemp.getId() ) ){
+				if (theVan.getVehicleRegistration().equals(vanTemp.getVehicleRegistration())){
+					isValidRegistration = false;break;
+				}
 			}
+		}
+		if ( theVan.getVehicleRegistration().equals("Please Enter a Unique VehicleReg #") || theVan.getVehicleRegistration().equals("Please Enter a Valid VehicleReg #") ){
+			theVan.setVehicleRegistration("Please Enter a Valid VehicleReg #");
 			
+			errors = true;
 		}
-		
 		if (!isValidRegistration){
-			theVan.setVehicleRegistration("Please enter a Unique VehicleReg #.");
+			theVan.setVehicleRegistration("Please Enter a Unique VehicleReg #");
 		
-						errors = true;
+			errors = true;
 		}
 		
+		
+	 	// Check for Vehicle Category / Van Type
 		List<VanType> vanTypes = vanTypeService.getVanTypes();
 		
 		boolean isValidCategory = false;
-		
-		
 		
 		for (VanType vanType : vanTypes){
 			if (vanType.getType().equals(theVan.getVanTypeCategory()) ) {
@@ -104,15 +110,38 @@ public String saveVan(@ModelAttribute("van") Van theVan){
 				
 		}
 		if (!isValidCategory ){
-				theVan.setVanTypeCategory("Please enter a valid Category.");
+				theVan.setVanTypeCategory("Please Enter a Valid Category");
 					errors = true;
-			}
+		}
+		
+		
+		// Empty Field Checks
+		//Vehicle Registration
+		if ( theVan.getVehicleRegistration().equals("") || theVan.getVehicleRegistration().equals("Please Enter a Valid VehicleReg #") ){
+			theVan.setVehicleRegistration("Please Enter a Valid VehicleReg #");
+			errors = true;
+		}
+		//Vehicle Type
+		if ( theVan.getVehicleType().equals("") || theVan.getVehicleType().equals("Please Enter a Valid Vehicle Type") ){
+			theVan.setVehicleType("Please Enter a Valid Vehicle Type");
+			errors = true;
+		}
+		//Total Seats
+		if ( theVan.getTotalSeats() < 1 ){
+			theVan.setTotalSeats(-1);
+			errors = true;
+		}
+		//Vehicle Category / Van Type
+		if ( theVan.getVanTypeCategory().equals("") || theVan.getVanTypeCategory().equals("Please Enter a Valid Category") ){
+			theVan.setVanTypeCategory("Please Enter a Valid Category");
+			errors = true;
+		}
+		
 					
+		if (errors){
 			
-			if (errors){
-				
-				return "van-form";
-			}
+			return "van-form";
+		}
 		
 
 	vanService.saveVan(theVan);
@@ -127,7 +156,11 @@ public String saveVan(@ModelAttribute("van") Van theVan){
 @GetMapping("/showFormForUpdate")
 public String showFormForUpdate (@RequestParam("vanId") int theId, Model theModel){
 	
+	isUpdate = true;
+	
 	Van theVan = vanService.getVan(theId);
+	
+	theVan.setVanTypeCategory( theVan.getCategory().getType() );
 	
 	theModel.addAttribute("van", theVan);
 	
